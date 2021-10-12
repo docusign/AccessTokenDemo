89d5dc4c0628d29786212ef65a2daad75c9a0954
Aaron JacksonWilde <Aaron.Wilde@docusign.com>
2021-10-12 11:13:18 -0700

    added clickwrap and envelope api example calls

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 // call in the DocuSign eSignature API
 let docusign = require("docusign-esign");
-// call the DocuSign Admin API
+// call in the DocuSign Admin API
 let adminApi = require("docusign-admin");
+// call in the DocuSign Click API
+let clickApi = require("docusign-click");
+
 // call in fs (filesystem) module to point to a file on our hard disk
-let fs = require("fs");
+let fs = require("fs/promises");
+
+// call in the process.exit() function to stop the script for error handling
+const { exit } = require("process");
 
 // These are the variables we intend to set using today's javascript demo.
-var accessToken, expiry, accountId, organizationId;
+var accessToken, expiry, accountId, organizationId, clickwraps, envelopes;
 
 //  These are on your apps and keys page at https://developers.docusign.com 
 let impersonationUserGuid = "";
 let integrationKey = "";
 
 // 'signature' for eSignature, organization_read to retreive your OrgId 
-let scopes = "signature+organization_read";
+let scopes = "signature+organization_read+click.manage";
 
 // This is also set for for specific Integration key, found on the Apps and Keys page
 let redirectUri = "https://httpbin.org/get";
 
 // NOTE: change this to account.docusign.com for production
 let oAuthBasePath = "account-d.docusign.com"
+
+// NOTE: change this to https://docusign.net/ for production
+let ApiBasePath = "https://demo.docusign.net";
 
 
 // The a bit of a gotcha going on here.  When providing scopes for generating your access token, specifing 'impersonation' is not necessary
@@ -38,25 +47,40 @@ DS.getJWT = async function _getJWT() {
         let apiClient = new docusign.ApiClient();
         apiClient.setOAuthBasePath(oAuthBasePath);
 
-        let privateKey = fs.readFileSync("private.key", "utf8");
+        let privateKey = await fs.readFile("private.key", "utf8");
 
         // Let's get an access token
         let response = await apiClient.requestJWTUserToken(integrationKey, impersonationUserGuid, scopes, privateKey, 3600);
 
-        // show the API response
+        // Show the API response 
         console.log(response.body);
 
         // Save the expiration time and accessToken variables
         expiry = response.body.expires_in;
         accessToken = response.body.access_token;
 
+        // Accessible JSON from module exports
+        return { "expiry" : expiry, "accessToken" : accessToken};
 
-    } catch (error) {
+    } catch (err) {
+        // Let's check if there's even a response body before trying to parse it
+       if (err.response){      
         // The time spent to find this line that took me more effort than I'd like to admit
-        if (error.response.body.error == "consent_required") {
+
+        if (err.response.body.error == "consent_required") {
             console.log("Consent required");
-            // Interesting quirk - any user can grant consent for their user GUID through your integration key's URL
+            // Interesting quirk - any user can grant consent for
+            // their user GUID through your integration key's URL
             console.log("Consent URL: " + consentUrl);
+
+            // Exit since we cannot run further API calls
+            exit(0);
+        }
+     } else {
+
+            // Something else has gone wrong, let's halt execution further
+            console.error(err);
+            exit(0);
         }
     }
 }
@@ -77,9 +101,13 @@ DS.getUserInfo = async function _getUserInfo(accessToken) {
 
         // Save the API Account ID to a variable
         accountId = response.accounts[0].accountId
-    } catch (error) {
-        throw error;
-    }
+
+        // Accessible JSON from module exports
+        return { "accountId" : accountId } ;
+
+    }  catch (err) {
+        console.error(err);
+    };
 };
 
 // Sets the Organziation ID variable
@@ -103,6 +131,9 @@ DS.getOrgId = async function _getOrgId(accessToken) {
         // Save the Organization ID to a variable IF we belong to an organization
         if (response.organizations.length > 0 ){
             organizationId = response.organizations[0].id;
+
+            // Accessible JSON from module exports
+            return { "organizationId" : organizationId };
         }
         else {
             console.log("User does not belong to an organization");
@@ -110,9 +141,55 @@ DS.getOrgId = async function _getOrgId(accessToken) {
         
 
 
-    } catch (error) {
-        throw error;
+    } catch (err) {
+        console.error(err);
     };
+};
+
+DS.getEnvelopes = async function _getEnvelopes(accessToken, accountId) {
+    try {
+        let apiClient = new docusign.ApiClient();
+        apiClient.setBasePath(ApiBasePath + "/restapi");
+        apiClient.addDefaultHeader("Authorization", `Bearer ${accessToken}`);
+
+        let envelopesApi = new docusign.EnvelopesApi(apiClient);
+        const msSinceEpoch = (new Date()).getTime();
+        // 720 hours OR 30 days ago
+        const thirtyDaysAgo = new Date(msSinceEpoch - 720 * 60 * 60 * 1000).toISOString();
+        let options = {fromDate: thirtyDaysAgo};
+
+        let response = await envelopesApi.listStatusChanges(accountId, options);
+
+        // Show the API response
+        console.log(response);
+
+        envelopes = response.body;
+
+        return envelopes;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+DS.getClickwraps = async function _getClickwraps(accessToken, accountId) {
+    try {
+        let apiClient = new clickApi.ApiClient();
+        apiClient.setBasePath(ApiBasePath + "/clickapi");
+        apiClient.addDefaultHeader("Authorization", `Bearer ${accessToken}`);
+
+        let accounts = new clickApi.AccountsApi(apiClient);
+
+        let response = await accounts.getClickwraps(accountId);
+
+        // Show the API response
+        console.log(response);
+
+        clickwraps = response.body
+
+        return clickwraps;
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 
@@ -121,17 +198,23 @@ DS.getOrgId = async function _getOrgId(accessToken) {
     await DS.getJWT();
     await DS.getUserInfo(accessToken);
     await DS.getOrgId(accessToken);
-
+    await DS.getEnvelopes(accessToken, accountId);
+    await DS.getClickwraps(accessToken, accountId);
 })();
 
 
 
 
 // ****************************************** 
-// If your intention is to use this code in an export,
+// OR - If your intention is to use this code in an export,
 // comment out the IIFE above and this instead: 
 
-// export default DS;
+// module.exports.DS = DS;
+
+// THEN try this in your terminal REPL or your external file:
+
+// const DS = require("./index.js");
+// DS.DS.getJWT().then( done => {console.log(done.accessToken)});
 
 // ******************************************
 
@@ -142,7 +225,7 @@ DS.getOrgId = async function _getOrgId(accessToken) {
 
 ******************************************
  LONGHANDED CALLBACK oldschool sort-of way. 
- IMO IIFE (as above) is easier to read!
+ IIFE (as above) is easier to read!
 
  JS is non blocking which means callbacks, promises,
  or async/await. I went async/await, but this 
